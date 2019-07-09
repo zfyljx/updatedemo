a33f280b8b049de13fce787426449e19e133a100
hxq <2305461452@qq.com>
2019-07-09 17:56:54 +0800

    Initial commit

---
+++ src/main/java/com/hxq/updatedemo/FileController.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: FileController
+ * Author:   hxq
+ * Date:     2019/7/9 17:35
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.hxq.updatedemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author hxq
+ * @create 2019/7/9
+ * @since 1.0.0
+ */
+@Controller
+public class FileController {
+
+    @GetMapping(value = "/file")
+    public String file() {
+        return "file";
+    }
+
+    @PostMapping(value = "/fileUpload")
+    public String fileUpload(@RequestParam(value = "file") MultipartFile file, Model model, HttpServletRequest request) {
+        if (file.isEmpty()) {
+            System.out.println("文件为空空");
+        }
+        String fileName = file.getOriginalFilename();  // 文件名
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));  // 后缀名
+        String filePath = "D://temp-rainy//"; // 上传后的路径
+        fileName = UUID.randomUUID() + suffixName; // 新文件名
+        File dest = new File(filePath + fileName);
+        if (!dest.getParentFile().exists()) {
+            dest.getParentFile().mkdirs();
+        }
+        try {
+            file.transferTo(dest);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String filename = "/temp-rainy/" + fileName;
+        model.addAttribute("filename", filename);
+        //System.out.println(filename);
+        return "show";
+    }
+
+}
+
---
+++ src/main/java/com/hxq/updatedemo/UpdatedemoApplication.java
@@ -0,0 +1,13 @@
+package com.hxq.updatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdatedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdatedemoApplication.class, args);
+	}
+
+}
